6826194c9c1acc0b56f7ad8f23e5b1436b675ff7
hamzahdili2001 <hdilihamza955@gmail.com>
2024-04-22 17:21:10 +0100

    const instead of let -> task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   // eslint-disable-next-line
-  for (let value of array) {
+  for (const value of array) {
     // eslint-disable-next-line
     array[array.indexOf(value)] = appendString + value;
   }
