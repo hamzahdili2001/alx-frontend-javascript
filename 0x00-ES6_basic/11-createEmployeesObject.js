e08088394f10698434d57b539a8c6cfe87e5e12e
hamzahdili2001 <hdilihamza955@gmail.com>
2024-04-22 17:39:57 +0100

    solve task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,11 +1,3 @@
-export default function createIteratorObject(report) {
-  const result = [];
-  // eslint-disable-next-line
-  for (const dep of Object.keys(report.allEmployees)) {
-    // eslint-disable-next-line
-    for (const e of report.allEmployees[dep]) {
-      result.push(e);
-    }
-  }
-  return result;
+export default function createEmployeesObject(departmentName, employees) {
+  return { [departmentName]: employees };
 }
