c4e45ee85c5db284ce778d9626f60cfec534afca
hamzahdili2001 <hdilihamza955@gmail.com>
2024-04-23 16:49:26 +0100

    solve task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+  const successMsg = {
+    status: 200,
+    body: 'Success',
+  };
+  const errorMsg = 'The fake API is not working currently';
+
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve(successMsg);
+    } else {
+      reject(errorMsg);
+    }
+  });
+}
