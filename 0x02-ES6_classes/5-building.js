a3fd08bc20c799d122cce23f62a60adf0eb274e8
hamzahdili2001 <hdilihamza955@gmail.com>
2024-04-25 16:32:47 +0100

    solve task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,10 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.evacuationWarningMessage === undefined && this.constructor !== Building) throw Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+  get sqft() { return this._sqft; }
+
+  set sqft(newSqft) { this._sqft = newSqft; }
+}
