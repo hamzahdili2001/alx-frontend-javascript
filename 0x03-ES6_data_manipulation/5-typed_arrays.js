891128b4c04a3760f7d4175a2e5bb3c0cf7276b9
hamzahdili2001 <hdilihamza955@gmail.com>
2024-04-29 16:10:34 +0100

    change Int8Array to DataView -> task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,9 +2,9 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   if (position > length - 1) throw Error('Position outside range');
 
-  const int8View = new Int8Array(buffer);
+  const int8View = new DataView(buffer, 0);
 
-  int8View[position] = value;
+  int8View.setInt8(position, value);
 
-  return buffer;
+  return int8View;
 }
